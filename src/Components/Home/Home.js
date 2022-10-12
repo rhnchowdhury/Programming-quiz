d0780ff47dfde3c4353ce6f26af833eb0f722b3c
Raihan <rhnchdri@gmail.com>
2022-10-12 22:10:11 +0600

    home img design

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,20 +2,25 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Quiz from '../Quiz/Quiz';
 import Row from 'react-bootstrap/Row';
-// import bannerImage from '../../../public/bg.jpg'
-
+import bannerImage from '../../../src/image/bg.jpg';
+import './Home.css'
 const Home = () => {
     const quiz = useLoaderData();
     console.log(quiz)
     return (
 
         <div>
-            <div className='dd'>
-                {/* <img src={bannerImage} alt="" /> */}
 
+            <div className='design'>
 
+                <div >
+                    <img src={bannerImage} style={{ width: 400 }} className='img-design' alt="" />
+                </div>
+                <div className='re-design'>
+                    <h3 className='mt-5 text-secondary'>React is a JavaScript library used to develop interactive UI. I create a quiz website to understand the basics of react.js, css, javascript.</h3>
 
-                <h3>po</h3>
+                </div>
+
             </div>
 
             <Row xs={1} md={2} lg={4} className='m-5'>
