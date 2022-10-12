dc7f4f2fd1b4eedaae60a2c83b74a8083f511757
Raihan <rhnchdri@gmail.com>
2022-10-12 14:42:27 +0600

    added toast

--- src/Components/QsAns/QsAns.js
+++ src/Components/QsAns/QsAns.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const QsAns = ({ op, correctAnswer }) => {
 
-
+    //*********Quiz Toasted******//
     const popUpToast = (op, right) => {
         if (op === right) {
-            alert('right')
+            toast.success('Your answer is correct !!!', {
+                position: toast.POSITION.TOP_CENTER
+            });
         }
         else {
-            alert('false')
+            toast.error('Your answer is wrong !!! ', {
+                position: toast.POSITION.TOP_CENTER
+            });
         }
     }
+    /**************** */
 
     return (
         <div className='d-flex justify-content-center  m-2'>
-            <Button onClick={() => popUpToast(op, correctAnswer)} variant="outline-secondary w-50">{op}</Button>
+            <Button onClick={() => popUpToast(op, correctAnswer)} variant="outline-secondary w-50">{op}</Button><ToastContainer />
         </div>
     );
 };
